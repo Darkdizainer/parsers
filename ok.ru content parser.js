b77afc53fa7e97fe12cf9eb45a865e7408c366e4
Darkdizainer <77378802+Darkdizainer@users.noreply.github.com>
2021-01-13 10:10:16 +0300

    Create ok.ru content parser.js

---
+++ ok.ru content parser.js
@@ -0,0 +1,33 @@
+/*******************
+* Парсер контента для Одноклассников ok.ru, скрипт выводит все новости заменяя страницу одноклассников, показывает название, репосты, комменты и ссылку на пост.
+*
+*
+*
+**********************/
+
+let posts = document.querySelectorAll('.feed-w');
+console.log (`Итого постов: ${posts.length}`)
+
+for (i = 0; i <= posts.length; i++){
+    let a = posts[i].querySelectorAll('.js-count');
+    let baseURL = posts[i].querySelectorAll('a');
+    let textBlock = posts[i].querySelector('.media-text_cnt');
+
+    // Отсекаем Null
+    if (textBlock == null) {
+        textBlock = 'НЕТУ ТЕКСТА, скорее всего видосик';
+    }
+
+    // Условия вывода постов
+    if (a[1].textContent >= 30){
+        document.write (`/*****************************************  ${i + 1}  ******************************************/<br><br>
+        Заголовок: ${textBlock.textContent}<br>
+        Коментов:   ${a[0].textContent}<br>
+        Репостов:  ${a[1].textContent}<br>
+        Лайков:  ${a[2].textContent}<br>
+        Ссылка:  <a href='${baseURL[2]}' target="_blank"> ${baseURL[2]} </a><br>
+        <br>
+        `);
+    }
+
+};
