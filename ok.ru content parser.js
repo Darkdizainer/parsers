2301c2980b3d72a7398068fa3914a64618fce633
Darkdizainer <77378802+Darkdizainer@users.noreply.github.com>
2021-01-19 01:00:08 +0300

    Update ok.ru content parser.js

--- ok.ru content parser.js
+++ ok.ru content parser.js
@@ -1,33 +1,37 @@
 /*******************
 * Парсер контента для Одноклассников ok.ru, скрипт выводит все новости заменяя страницу одноклассников, показывает название, репосты, комменты и ссылку на пост.
-* v 1.0 (01.21)
+* v 1.1 (01.21)
 *
 *
 **********************/
 
-let posts = document.querySelectorAll('.feed-w');
+llet posts = document.querySelectorAll('.feed-w');
+let arrPost = [];
 console.log (`Итого постов: ${posts.length}`)
 
-for (i = 0; i <= posts.length; i++){
+// Разбираем новости на элементы (заголовок, лайки, репосты и так далее)
+for (i = 0; i < posts.length; i++){
     let a = posts[i].querySelectorAll('.js-count');
     let baseURL = posts[i].querySelectorAll('a');
     let textBlock = posts[i].querySelector('.media-text_cnt');
+    // Загоняем в массив объекты с данными
+    arrPost.push({text: textBlock.textContent, komm: a[0].textContent, repost: a[1].textContent, like: a[2].textContent, link: baseURL[2]},);
+};
 
-    // Отсекаем Null
-    if (textBlock == null) {
-        textBlock = 'НЕТУ ТЕКСТА, скорее всего видосик';
-    }
+//Сортируем массив от большего к меньшему по репостам (repost) или лайкам (like) или комментам (komm) нужно раскоментировать
+arrPost.sort(function(a, b){
+    //return b.repost-a.repost
+    return b.like-a.like
+    //return b.komm-a.komm
+});
 
-    // Условия вывода постов
-    if (a[1].textContent >= 30){
-        document.write (`/*****************************************  ${i + 1}  ******************************************/<br><br>
-        Заголовок: ${textBlock.textContent}<br>
-        Коментов:   ${a[0].textContent}<br>
-        Репостов:  ${a[1].textContent}<br>
-        Лайков:  ${a[2].textContent}<br>
-        Ссылка:  <a href='${baseURL[2]}' target="_blank"> ${baseURL[2]} </a><br>
-        <br>
-        `);
-    }
-
+//Вывод на экран результатов
+for (ii=0;ii<arrPost.length;ii++){
+    document.write (`/*****************************************  ${ii + 1}  ******************************************/<br><br>
+    Заголовок: ${arrPost[ii].text}<br>
+    Коментов:   ${arrPost[ii].komm}<br>
+    Репостов:  ${arrPost[ii].repost}<br>
+    Лайков:  ${arrPost[ii].like}<br>
+    Ссылка:  <a href='${arrPost[ii].link.href}' target="_blank"> ${arrPost[ii].link} </a><br>
+    <br>`)
 };
